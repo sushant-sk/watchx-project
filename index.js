711262fc015aabc91953169c745b47f2181e8ec3
sushant-sk <97774364+sushant-sk@users.noreply.github.com>
2023-07-10 01:52:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+document.querySelector("").style.backgroundColor="red";
+
+
+
+
+function hover_to_brown(k) {
+  k.style.color = "brown";
+}
+
+
+
+function hover_on_card(k){
+    k.style.backgroundColor="black";
+    k.style.color="white";
+}
+
+function hover_to_back(k){
+    k.style.color="black";
+    k.style.backgroundColor="white";
+}
